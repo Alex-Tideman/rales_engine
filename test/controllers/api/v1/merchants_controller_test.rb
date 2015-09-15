ec83b219ae4fe11e9efd872bda9a61e52de3755c
Alex Tideman <alex.tideman@gmail.com>
2015-09-15 09:58:21 -0600

    Remove test helpers

---
+++ test/controllers/api/v1/merchants_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class Api::V1::MerchantsControllerTest < ActionController::TestCase
+  test "#index" do
+    get :index, format: :json
+
+    items = JSON.parse(response.body)
+    item = items.last
+
+    assert_response :success
+    assert_equal 2, items.count
+    assert_equal 'Jon Snow', item["name"]
+  end
+end
